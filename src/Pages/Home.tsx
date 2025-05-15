a747a851f95e6cb970fbd8d944a2ed173ad56f40
Ibeneme <ibenemeikenna96@gmail.com>
2025-05-15 16:23:34 +0100

    new ui

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,14 @@
 import Hero from "./Hero/Hero";
 import AboutUs from "../Components/AboutUs/AboutUs";
 import FAQPage from "../Components/Faqs/FAQPage";
-import AccordionSection from "../Components/AccordionSection/AccordionSection";
+// import AccordionSection from "../Components/AccordionSection/AccordionSection";
 
 const Home = () => {
   return (
     <div style={{ margin: 0, padding: 0 }}>
       <Hero />
       <AboutUs />
-      <AccordionSection />
+      {/* <AccordionSection /> */}
       <FAQPage />
 
     </div>
