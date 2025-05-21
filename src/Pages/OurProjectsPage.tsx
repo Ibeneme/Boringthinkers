5ee67a39bbf4f09d3b8fcf5d4a86737934e4c861
Ibeneme <ibenemeikenna96@gmail.com>
2025-05-21 18:31:33 +0100

    npm run build

--- src/Pages/OurProjectsPage.tsx
+++ src/Pages/OurProjectsPage.tsx
@@ -1,10 +1,11 @@
+
 import BlogHero from "../Components/BlogHero/BlogHero";
 import OurProjectComponent from "../Components/Projects/Projects";
 
 const OurProjectsPage = () => {
   return (
     <div>
-      <BlogHero title="Sept 2024' - Today 25" />
+      <BlogHero title={`2024" - 25`} />
       <OurProjectComponent />
     </div>
   );
