87886f26fcca9ee161ba2696e24643a0dd715ca2
Ibeneme <ibenemeikenna96@gmail.com>
2025-05-21 08:29:32 +0100

    npm run build

--- src/Pages/OurProjectsPage.tsx
+++ src/Pages/OurProjectsPage.tsx
@@ -5,7 +5,7 @@ import OurProjectComponent from "../Components/Projects/Projects";
 const OurProjectsPage = () => {
   return (
     <div>
-      <BlogHero title="OUR MOST RECENT PROJECTS" />
+      <BlogHero title="2024' - 25" />
       <OurProjectComponent />
     </div>
   );
