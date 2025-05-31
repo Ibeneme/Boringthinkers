b9aa7f74e3ca0933610572a049f2020ce951bf9c
Ikenna-apna <ikenna@apnadevs.com>
2025-05-31 16:51:14 +0100

    moved projects

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,6 +5,7 @@ import OurCompany from "./OurCompany/OurCompany";
 import We from "../Components/We/We";
 import AccordionSection from "../Components/AccordionSection/AccordionSection";
 import SlidingPage from "../Components/Footer/SlidingPage";
+import NewProjects from "../Components/NewProjects/Projects";
 // import AccordionSection from "../Components/AccordionSection/AccordionSection";
 
 const Home = () => {
@@ -15,6 +16,7 @@ const Home = () => {
       <OurCompany />
       <SlidingPage />
       <We />
+      <NewProjects />
       <div style={{ padding: 16, width: "100vw" }}>
         <AccordionSection />
       </div>
