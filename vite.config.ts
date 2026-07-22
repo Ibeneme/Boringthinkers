9a6897168ef32ca70d94f2dd3f91ab3ee7c7341b
ibeneme <ikennaibenemee@gmail.com>
2026-07-22 14:38:41 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-  // Ensure public folder files are properly handled
-  publicDir: 'public',
-
-  build: {
-    // This helps with static asset serving
-    assetsDir: 'assets',
-    // Generate clean sourcemaps (optional)
-    sourcemap: false,
-  },
-
-  // Optional: Better base path (important for custom domains)
-  base: '/',
-
-
+  plugins: [
+    react(),
+    //    tailwindcss()
+  ]
 })
